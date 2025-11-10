ff0535285654b9d29c5936a9c489f17b5ec60ecd
Jhn63 <jhn.vt.1998@gmail.com>
2025-11-10 19:15:06 -0300

    feat: adiciona configurações cords

--- backend/main.go
+++ backend/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -19,6 +20,12 @@ func main() {
 	}
 
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:3000, http://localhost:5173",
+		AllowMethods: "GET, POST, PUT, DELETE",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+
 	app.Get("/tasks/:id", th.GetTask)
 	app.Post("/post", th.CreateTask)
 	app.Patch("/tasks/:id", th.UpdateTask)
